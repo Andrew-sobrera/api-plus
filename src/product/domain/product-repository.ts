91fe964fe6cfcb7ebb0bfc7c8fddc4a546c0fa89
andrew-sobrera-bagy <andrew.sobrera@bagy.com.br>
2024-07-31 17:47:56 -0300

    task: crud produtos

--- src/product/domain/product-repository.ts
+++ src/product/domain/product-repository.ts
@@ -1,7 +1,14 @@
-import { Product } from "./product"
+import { ID } from "../../common/product/types"
+import { Product, ProductCreate } from "./product"
 
 export const PRODUCT_REPOSITORY = 'ProductRepository'
 
 export interface ProductRepository {
     getAll(): Promise<Product[]>
+
+    getOne(id: number): Promise<Product | null>
+
+    create(product: ProductCreate): Promise<Product>
+
+    update(product: ProductCreate, id: ID): Promise<Product>
 }
--- src/product/domain/product-service.ts
+++ src/product/domain/product-service.ts
@@ -1,6 +1,7 @@
 import { container, inject, injectable } from 'tsyringe'
 import { PRODUCT_REPOSITORY, ProductRepository } from './product-repository'
-import { Product } from './product'
+import { Product, ProductCreate } from './product'
+import { ID } from '../../common/product/types'
 
 export const PRODUCT_SERVICE = 'ProductService'
 
@@ -18,5 +19,20 @@ export class ProductService {
     async getAll():Promise<Product[]>{
         const products = await this.productRepository.getAll()
         return products
+    }
+
+    async getOne(id: any): Promise<Product | null>{
+        const product = await this.productRepository.getOne(id)
+        return product
+    }
+
+    async create(product: ProductCreate): Promise<Product>{
+        const productCreate = await this.productRepository.create(product)
+        return productCreate
+    }
+
+    async update(product: ProductCreate, id: ID): Promise<Product>{
+        const productUpdate = await this.productRepository.update(product, id)
+        return productUpdate
     }
 }
